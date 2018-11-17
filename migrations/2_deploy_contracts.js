0738f3c9bc45c06aa63aa8283b27ed1de015d8cd
Weihao Cao <github@weihaocao.com>
2018-11-17 19:19:47 +0800

    Spelling error

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 var Wallet = artifacts.require("./Wallet.sol");
-var Contract = artifacts.require("./Contract.sol");
+var Channel = artifacts.require("./Channel.sol");
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
-  deployer.deploy(Contract);
-  deployer.link(Contract,Wallet);
+  deployer.deploy(Channel);
+  deployer.link(Channel,Wallet);
   deployer.deploy(Wallet);
 };
