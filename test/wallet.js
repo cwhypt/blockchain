ce8a8b960149c8d5534861e12f5b1cc85b9ff13e
Weihao Cao <github@weihaocao.com>
2018-11-18 19:53:34 +0800

    Update wallet.js

--- test/wallet.js
+++ test/wallet.js
@@ -10,8 +10,18 @@ contract('wallet', function(accounts) {
       assert.equal(balance.valueOf(), 100, "100 wasn't in the first account");
 	  return wal.getValue.call(accounts[1]);
     }).then(function(balance) {
-      assert.equal(balance.valueOf(), 10, "10 wasn't in the second account or undefined");
+      assert.equal(balance.valueOf(), 0, "0 wasn't in the second account or undefined");
     });
   });
+  
+  it("check the initialization of mappings", function() {
+    var wal;
+
+    return wallet.deployed().then(function(instance) {
+      wal = instance;
+      return wal.getChannel.call(accounts[0]);
+    }).
+  });
+  
   
 });
