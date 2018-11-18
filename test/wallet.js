0dfa9fbbcfa208395528044dce98b50e528af24c
Weihao Cao <github@weihaocao.com>
2018-11-18 19:40:16 +0800

    Update wallet.js

--- test/wallet.js
+++ test/wallet.js
@@ -2,10 +2,15 @@ var wallet = artifacts.require("./Wallet.sol");
 
 contract('wallet', function(accounts) {
   it("should set up channelUser for the first account", function() {
+	  var wal;
     return wallet.deployed().then(function(instance) {
-      return instance.getValue.call(accounts[0],{from: account[0]});
+	  wal=instance;
+      return wal.getValue.call(accounts[0],{from: accounts[0]});
     }).then(function(balance) {
-      assert.equal(balance.valueOf(), 100, "10000 wasn't in the first account");
+      assert.equal(balance.valueOf(), 100, "100 wasn't in the first account");
+	  return wal.getValue.call(accounts[1]);
+    }).then(function(balance) {
+      assert.equal(balance.valueOf(), 10, "10 wasn't in the second account or undefined");
     });
   });
   
