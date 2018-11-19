0f261be6ed2c3eaa77c879d5353b7767bf9a7d50
Weihao Cao <github@weihaocao.com>
2018-11-19 23:02:05 +0800

    silly error

--- contracts/Channel.sol
+++ contracts/Channel.sol
@@ -25,7 +25,7 @@ contract Channel {
 		value=0;
 	}
 
-	function openChannel(string _name, address _to, uint _timeout) payable returns (uint){ 
+	function openChannel(string _name, address _to, uint _timeout) payable returns (string){ 
 		//address to, uint timeout
 		name=_name;
 		to=new address[](1);
